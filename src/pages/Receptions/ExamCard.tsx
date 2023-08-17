2f9c34978ee11376f099e11e99fd60f64ecf0657
SJanna <jannas@uninorte.edu.co>
2023-08-17 17:00:01 -0400

    Exam card selected color changed

--- src/pages/Receptions/ExamCard.tsx
+++ src/pages/Receptions/ExamCard.tsx
@@ -19,8 +19,8 @@ const ExamCard = ({ name }: ExamCardProps) => {
 
     <button
       onClick={() => setIsSelected(!isSelected)}
-      className={`max-w-40 w-60 h-25 font-bold py-2 px-4 rounded inline-flex items-center shadow-body gap-1 hover:bg-whiten dark:bg-boxdark dark:hover:bg-secondary dark:text-whiten dark:hover:text-white ${isSelected ? 'relative bottom-2 left-2 shadow-md shadow-black dark:bg-primary' : 'shadow-sm'}`}>
-      {isSelected ? <IoIosCheckmarkCircle className={`text-primary dark:text-white`}/> : <IoIosCheckmarkCircleOutline />}
+      className={`max-w-40 w-60 h-25 font-bold py-2 px-4 rounded inline-flex items-center shadow-body gap-1 hover:bg-whiten dark:bg-boxdark dark:hover:bg-secondary dark:text-whiten dark:hover:text-white ${isSelected ? 'relative bottom-2 shadow-md shadow-black dark:bg-primary bg-primary text-white hover:bg-primary' : 'shadow-sm'}`}>
+      {isSelected ? <IoIosCheckmarkCircle className={`text-primary dark:text-white text-white`}/> : <IoIosCheckmarkCircleOutline />}
       
       <span>{name}</span>
     </button>
